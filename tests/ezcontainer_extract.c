8decd6b34bfba8c4c2cf9b8bfe69d8af97f6d59b
George Watson <gigolo@hotmail.co.uk>
2023-12-09 13:02:53 +0000

    updated old test file to build

--- tests/ezcontainer_extract.c
+++ tests/ezcontainer_extract.c
@@ -4,13 +4,13 @@
 int main(int argc, const char* argv[]) {
     assert(argc == 2);
     ezContainer *tree = ezContainerRead(argv[1]);
-    
+
     ezContainerTreeEntry *e = NULL;
     for (int i = 0; i < tree->sizeOfEntries; ++i) {
         e = &tree->entries[i];
         char buf[255];
-        sprintf(buf, ".%s", strrchr(e->fp, '/'));
-        
+        sprintf(buf, ".%s", strrchr(e->filePath, '/'));
+
         unsigned char* data = ezContainerEntryRaw(tree, &e->entry);
         FILE* tmp = fopen(buf, "w");
         fwrite(data, e->entry.fileSize * sizeof(unsigned char), 1, tmp);
