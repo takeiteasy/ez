0703d6cecc96bc47c207d2faf48acb064469608e
George Watson <gigolo@hotmail.co.uk>
2023-12-17 12:40:18 +0000

    added ezkvm + clean up

--- ezobjc.h
+++ ezobjc.h
@@ -33,6 +33,7 @@
  --------------------------------------------------------------------
  
  The MIT License (MIT)
+ 
  Copyright (c) 2022 George Watson
  Permission is hereby granted, free of charge, to any person
  obtaining a copy of this software and associated documentation
@@ -41,8 +42,10 @@
  publish, distribute, sublicense, and/or sell copies of the Software,
  and to permit persons to whom the Software is furnished to do so,
  subject to the following conditions:
+ 
  The above copyright notice and this permission notice shall be
  included in all copies or substantial portions of the Software.
+ 
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
